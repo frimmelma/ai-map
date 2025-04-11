94878356cb1c06eea4473bf052526c112101c89b
Matej Frimmel <frimmelmatej@gmail.com>
2025-04-11 20:36:36 +0200

    Fix deprecated dependencies and suppress installation warnings - v1 release

---
+++ ai-map/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest'],
+  rules: {
+    'no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'import/no-anonymous-default-export': 'off',
+  },
+};
--- ai-map/webpack.config.js
+++ ai-map/webpack.config.js
@@ -7,6 +7,34 @@ module.exports = {
     setupMiddlewares: (middlewares, devServer) => {
       // Your custom middleware setup can go here if needed
       return middlewares;
-    }
-  }
+    },
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+    hot: true,
+    liveReload: true,
+  },
+  // Disable performance hints
+  performance: {
+    hints: false,
+  },
+  // Silence deprecation warnings
+  ignoreWarnings: [
+    {
+      module: /mini-css-extract-plugin/,
+    },
+    {
+      module: /postcss-loader/,
+    },
+    {
+      module: /terser-webpack-plugin/,
+    },
+    {
+      module: /webpack-dev-server/,
+    },
+    (warning) => true,
+  ],
 };
